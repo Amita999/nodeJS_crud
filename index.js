4a09d3d1e196fb5c9a17bb59d7d20e022e34704e
Amita999 <amitajethani@gmail.com>
2021-05-05 14:34:51 +0530

    implementation for product page done

--- index.js
+++ index.js
@@ -1,24 +1,68 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
+let templateOverview = fs.readFileSync(`./templates/overview.html`, "utf-8");
+let templateCard = fs.readFileSync(`./templates/template-card.html`, "utf-8");
+let templateProduct = fs.readFileSync(`./templates/product.html`, "utf-8");
 let data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-
+ 
 let dataObject = JSON.parse(data);
-
+ 
+const replaceTemplateFunc = (temp, product) => {
+  // console.log(temp);
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+ 
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+// console.log(dataObject);
+ 
 //Creating a server
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
-  if (pathName == "/" || pathName == "/overview") {
-    console.log(pathName);
+  const {pathname,query} = url.parse(req.url,true);
 
-    // console.log(JSON.parse(data));
+  console.log("pathName: ", pathname);
+  console.log("Parsing the query parameter: ", url.parse(req.url,true));
+
+  
+  //overview page
+  if (pathname == "/" || pathname == "/overview") {
+    
     console.log("Inside home or overview route ");
-    res.writeHead(200, { "Content-type": "application/json" });
-    res.end(data);
+    const cardsHtml = dataObject.map((el) =>
+      replaceTemplateFunc(templateCard, el)
+    ).join(''); 
+    const output = templateOverview.replace('{%PRODUCT-CARD%}',cardsHtml)
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(output);
   }
-  // res.end("Hello from the server");
+  //product page
+  else if (pathname == "/product") {
+    console.log("Inside product page");
+    console.log("query: ",query);
+    const product = dataObject[query.id];
+    const output = replaceTemplateFunc(templateProduct,product);
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(output);
+  }
+  //page not found
+  else {
+    console.log("pathname: ", pathname);
+    console.log("Inside Not found");
+    res.writeHead(404, { "Content-type": "text/html" });
+    res.end("<h1>Page Not Found </h1>");
+  }
 });
-
+ 
 server.listen(8000, "127.0.0.1", () => {
   console.log("listening to requests");
 });
