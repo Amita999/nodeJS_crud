bc42af3c7d23d1f81341b9cf8b72fc42a7e43f29
Amita999 <amitajethani@gmail.com>
2021-05-05 14:46:45 +0530

    Created a new module and used it as exports

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
-
+const replaceTemplateFunc = require('./modules/replaceTemplateFunc');
 let templateOverview = fs.readFileSync(`./templates/overview.html`, "utf-8");
 let templateCard = fs.readFileSync(`./templates/template-card.html`, "utf-8");
 let templateProduct = fs.readFileSync(`./templates/product.html`, "utf-8");
@@ -9,21 +9,7 @@ let data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
  
 let dataObject = JSON.parse(data);
  
-const replaceTemplateFunc = (temp, product) => {
-  // console.log(temp);
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
- 
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
+
 // console.log(dataObject);
  
 //Creating a server
